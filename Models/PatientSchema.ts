5921fe3b37164e78568ba2e2ecf1f08c64a0ad0c
alexalmeida52 <jhosealex@gmail.com>
2021-08-12 15:21:48 -0300

    Model de patient criado

---
+++ Models/PatientSchema.ts
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Patient = mongoose.model('Patients', {
+  name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  gender: { type: String, required: true, enum: ['M', 'F'] },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  birthday: { type: Date, required: true },
+  created_at: { type: Date, default: Date.now }
+});
+
+export default Patient;
