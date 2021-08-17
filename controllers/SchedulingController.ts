77401514fc09dac785fc85ba17fc631f3bce5ebe
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 <jhosealex@gmail.com>
2021-08-16 21:17:25 -0300

    Controller de agendamentos

--- controllers/SchedulingController.ts
+++ controllers/SchedulingController.ts
@@ -1,33 +1,59 @@
 import { Request, Response } from 'express';
+import Scheduling from '../Models/SchedulingSchema';
 
 class SchedulingController {
     async create(req: Request, res: Response) {
+        let scheduling = new Scheduling(req.body);
 
-        return res.status(204).send();
+        try {
+            await scheduling.save();    
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(400).send('Failed to save');
+        }
     }
 
     async index(req: Request, res: Response) {
-
-        return res.json([]);
+        try {
+            let data = await Scheduling.find({});
+            return res.json(data);
+        } catch (error) {
+            return res.status(400).send('Failed to find');   
+        }
     }
 
     async show(req: Request, res: Response) {
         const { id } = req.params;
-
-        /* const item = await knex('items').where('id', id).select('*');
-
-        if(!item) return res.status(400).json({ message: "Item not found"});
-         */
-
-        return res.json({});
+        
+        try {
+            let schedulings = await Scheduling.findOne({ _id: id });
+            return res.json(schedulings);
+        } catch (error) {
+            console.log(error);
+            return res.status(400).send('Failed to find scheduling');
+        }
     }
 
     async update(req: Request, res: Response) {
-        return res.status(204).send();
+        const { id } = req.params;
+
+        try {
+            await Scheduling.updateOne({ _id: id }, req.body);
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(400).send('Failed to update scheduling');
+        }
     }
 
     async remove(req: Request, res: Response) {
-        return res.status(204).send();
+        const { id } = req.params;
+
+        try {
+            await Scheduling.deleteOne({ _id: id });
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(400).send('Failed to remove scheduling');
+        }
     }
 };
 
