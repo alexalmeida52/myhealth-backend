b5e62c7746b0f89c272ff4b468b00d4cdcfd90ca
alexalmeida52 <jhosealex@gmail.com>
2022-07-14 16:19:06 -0300

    Update table columns

--- Models/DoctorSchema.ts
+++ Models/DoctorSchema.ts
@@ -9,6 +9,9 @@ const Doctor = mongoose.model('Doctors', {
   birthday: { type: Date, required: true },
   clinic: { type: String, required: true },
   price: { type: Number, required: true },
+  lat: { type: String, required: true },
+  long: { type: String, required: true },
+  address: { type: String, required: true },
   created_at: { type: Date, default: Date.now }
 });
 
